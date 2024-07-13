82f74728cad854541464ccbcecc2dc3c506796e6
Zheliang Tan <zheliangtan@gmail.com>
2024-07-13 15:52:33 +0000

    test: token controller/service test

--- src/token/token.controller.spec.ts
+++ src/token/token.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokenController } from './token.controller';
+import { TokenService } from './token.service';
+import { Token } from './token.entity';
 
 describe('TokenController', () => {
   let controller: TokenController;
+  let mockTokenService = {
+    createMemberToken: jest.fn(),
+    createAdminToken: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TokenController],
+      providers: [
+        {
+          provide: TokenService,
+          useValue: mockTokenService,
+        },
+      ],
     }).compile();
 
     controller = module.get<TokenController>(TokenController);
@@ -14,5 +26,35 @@ describe('TokenController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('/token/member => create member token', () => {
+    it('should create a token object with accessToken property', async () => {
+      const token = { accessToken: '' } as Token;
+
+      jest
+        .spyOn(mockTokenService, 'createMemberToken')
+        .mockReturnValue({ accessToken: '' });
+
+      const result = await controller.createMemberToken();
+
+      expect(mockTokenService.createMemberToken).toHaveBeenCalled();
+      expect(result).toEqual(token);
+    });
+  });
+
+  describe('/token/admin => create admin token', () => {
+    it('should create a token object with accessToken property', async () => {
+      const token = { accessToken: '' } as Token;
+
+      jest
+        .spyOn(mockTokenService, 'createAdminToken')
+        .mockReturnValue({ accessToken: '' });
+
+      const result = await controller.createAdminToken();
+
+      expect(mockTokenService.createAdminToken).toHaveBeenCalled();
+      expect(result).toEqual(token);
+    });
   });
 });
--- src/token/token.service.spec.ts
+++ src/token/token.service.spec.ts
@@ -1,18 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokenService } from './token.service';
+import { JwtService } from '@nestjs/jwt';
+import { Token } from './token.entity';
 
 describe('TokenService', () => {
-  let service: TokenService;
+  let tokenService: TokenService;
+  let mockJwtService = { signAsync: jest.fn() };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TokenService],
+      providers: [
+        TokenService,
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
     }).compile();
 
-    service = module.get<TokenService>(TokenService);
+    tokenService = module.get<TokenService>(TokenService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(tokenService).toBeDefined();
+  });
+
+  describe('create member token', () => {
+    it('should return an object with accessToken property', async () => {
+      const signedJwt = {
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoibWVtYmVyIiwiaWF0IjoxNzIwODg0OTQ1LCJleHAiOjE3MjA4ODUyNDV9.q8rLURUuK_Vbtf2JQoIQtMTTESOBN-XdGOz6dXEynj0',
+      } as Token;
+
+      jest
+        .spyOn(mockJwtService, 'signAsync')
+        .mockReturnValue(
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoibWVtYmVyIiwiaWF0IjoxNzIwODg0OTQ1LCJleHAiOjE3MjA4ODUyNDV9.q8rLURUuK_Vbtf2JQoIQtMTTESOBN-XdGOz6dXEynj0',
+        );
+
+      const result = await tokenService.createMemberToken();
+
+      expect(mockJwtService.signAsync).toHaveBeenCalled();
+      expect(result).toEqual(signedJwt);
+    });
+  });
+
+  describe('create admin token', () => {
+    it('should return an object with accessToken property', async () => {
+      const signedJwt = {
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MjA4ODQ5NjAsImV4cCI6MTcyMDg4NTI2MH0.NgyI_ZV-5Ns4yv4YnahWSzXrYiGjg-iwZqVHZqookJk',
+      } as Token;
+
+      jest
+        .spyOn(mockJwtService, 'signAsync')
+        .mockReturnValue(
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MjA4ODQ5NjAsImV4cCI6MTcyMDg4NTI2MH0.NgyI_ZV-5Ns4yv4YnahWSzXrYiGjg-iwZqVHZqookJk',
+        );
+
+      const result = await tokenService.createAdminToken();
+
+      expect(mockJwtService.signAsync).toHaveBeenCalled();
+      expect(result).toEqual(signedJwt);
+    });
   });
 });
